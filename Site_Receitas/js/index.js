cd01bf3dcfd75c45aac7c947919bd9c6e27733b4
jorge luiz <jojoluiz42@gmail.com>
2024-04-17 10:58:38 -0300

    refactor:scroll-smooth

--- Site_Receitas/js/index.js
+++ Site_Receitas/js/index.js
@@ -1,13 +1,64 @@
-var onda1 = document.getElementById('onda1')
-var onda2 = document.getElementById('onda2')
-var onda3 = document.getElementById('onda3')
-var onda4 = document.getElementById('onda4')
+var onda1 = document.getElementById("onda1");
+var onda2 = document.getElementById("onda2");
+var onda3 = document.getElementById("onda3");
+var onda4 = document.getElementById("onda4");
 
-window.addEventListener('scroll', function(){
-    var rolagemPos = window.scrollY
+const menuItems = document.querySelectorAll('.navbar a[href^="#"]');
 
-    onda1.style.backgroundPositionX = 400 + rolagemPos * 4 + 'px';
-    onda2.style.backgroundPositionX = 300 + rolagemPos * -4 + 'px';
-    onda3.style.backgroundPositionX = 200 + rolagemPos * 2 + 'px';
-    onda4.style.backgroundPositionX = 100 + rolagemPos * -2 + 'px';
-})
+function getScrollTopByHref(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+function scrollToPosition(to) {
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  });
+  smoothScrollTo(0, to);
+}
+
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const to = getScrollTopByHref(event.currentTarget) - 80;
+  scrollToPosition(to);
+}
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", scrollToIdOnClick);
+});
+
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX;
+  const startY = window.scrollY;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== "undefined" ? duration : 1000;
+
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1)
+      return (distance / 2) * time * time * time * time + from;
+    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60);
+}
+
+window.addEventListener("scroll", function () {
+  var rolagemPos = window.scrollY;
+
+  onda1.style.backgroundPositionX = 400 + rolagemPos * 4 + "px";
+  onda2.style.backgroundPositionX = 300 + rolagemPos * -4 + "px";
+  onda3.style.backgroundPositionX = 200 + rolagemPos * 2 + "px";
+  onda4.style.backgroundPositionX = 100 + rolagemPos * -2 + "px";
+});
